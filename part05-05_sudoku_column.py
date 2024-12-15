dabd57f479303c7b134014bb0223e60096328423
LucaWalrus <luca.devecchis@gmail.com>
2024-12-15 20:16:07 +0100

    Create part05-05_sudoku_column.py

---
+++ part05-05_sudoku_column.py
@@ -0,0 +1,36 @@
+# Write your solution here
+
+def column_correct(sudoku: list, column_no: int):
+    check_list = []
+    singles = set()
+    duplos = set() 
+    for i in range(len(sudoku)):
+        row = sudoku[i]
+        column = row[column_no]
+        check_list.append(column)
+    for item in check_list:
+        if item in singles:
+            duplos.add(item)
+        else:
+            singles.add(item)
+    if duplos == {0}:
+        result = True
+    else:
+        result = False
+    return result
+
+
+if __name__ == "__main__":
+    sudoku = [
+        [9, 0, 0, 0, 8, 0, 3, 0, 0],
+        [2, 0, 0, 2, 5, 0, 7, 0, 0],
+        [0, 2, 0, 3, 0, 0, 0, 0, 4],
+        [2, 9, 4, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 7, 3, 0, 5, 6, 0],
+        [7, 0, 5, 0, 6, 0, 4, 0, 0],
+        [0, 0, 7, 8, 0, 3, 9, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 3],
+        [3, 0, 0, 0, 0, 0, 0, 0, 2]
+        ]
+    print(column_correct(sudoku, 0))
+    print(column_correct(sudoku, 1))
